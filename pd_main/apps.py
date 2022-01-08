8a132d02c6987e1a353426ec8afe06d4c39c1842
Coşkun Deniz <coskun.denize@gmail.com>
2022-01-08 12:12:46 +0300

    Upgrade to Django 4

--- pd_main/apps.py
+++ pd_main/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class PdMainConfig(AppConfig):
     name = 'pd_main'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         import pd_main.signals
--- python_doctor/urls.py
+++ python_doctor/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pd_main.urls')),
+    re_path(
+        r'^robots.txt$',
+        TemplateView.as_view(
+            template_name='pd_main/robots.txt', content_type='text/plain'
+        ),
+        name='robots_file',
+    )
 ]
 
 if settings.DEBUG:
